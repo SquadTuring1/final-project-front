fec992601328f7b697f4f4c7c4d87428005bc9a4
Jesus Herrera <heisusin@gmail.com>
2022-10-07 18:02:58 +0200

    perf: :zap: Added link and improve structure

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
-import { MainLogin, Btn, Logo, TextAccount, CenterArticle, TextColor } from '../../ui/index'
-import mainLogo from '../../assets/images/Logo-home.png'
-import {Link} from 'react-router-dom';
+import React from 'react';
+import {
+  MainLogin,
+  Btn,
+  Logo,
+  TextAccount,
+  CenterArticle,
+  TextColor,
+} from '../../ui/index';
+import mainLogo from '../../assets/images/Logo-home.png';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <MainLogin>
-    <Logo src={mainLogo} />
+      <Logo src={mainLogo} />
       <CenterArticle>
-        <Btn>Login</Btn>
-        <TextAccount>Don't have an account? <TextColor><Link to='registration'>Register</Link></TextColor></TextAccount>
+        <Link to="login">
+          <Btn>Login</Btn>
+        </Link>
+        <TextAccount>
+          Don't have an account?{' '}
+          <Link to="registration">
+            <TextColor>Register</TextColor>
+          </Link>
+        </TextAccount>
       </CenterArticle>
     </MainLogin>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
