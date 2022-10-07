38d0b842913defec0f12c29f656e00e5340d0ba7
Jesus Herrera <heisusin@gmail.com>
2022-10-07 13:30:07 +0200

    feat: :sparkles: Create routes

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Login from '../pages/Login/index'
+import Registration from '../pages/Registration/index'
+import Home from '../pages/Home/index'
+import { Routes, Route } from 'react-router-dom';
+import Navbar from '../components/Navbar/index'
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path='/' element={<Navbar />}>
+        <Route path='login' element={<Login />} />
+        <Route path='registration' element={<Registration />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default Router
