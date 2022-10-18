3d93445de706c140e29d91b8ed7092034c054be7
blakejohns5 <100588924+blakejohns5@users.noreply.github.com>
2022-10-18 22:33:27 +0200

    wip changing audio controls from component to reducer logic

--- src/features/songs/songsSlice.js
+++ src/features/songs/songsSlice.js
@@ -1,51 +1,70 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    currentSong: 0,
-    songsList : [],
+    currentSongId: 0,
+    currentSongIndex: 0,
+    songList : [],
     repeat: false,
-    random: false,
+    shuffle: false,
     playing: false,
 }
-
 
 const songsSlice = createSlice({
     name: 'songs',
     initialState,
     reducers: {
-        setSongsList: {
-            reducer(state, action){
-                return {...state, ...action.payload}
-            }
-        },
-        setCurrentSong: {
-            reducer(state, action) {
-                return {...state, currentSong: action.payload, playing: true}
-            }
-        },
-        setRandom: {
-            reducer(state, action) {
-                return {...state, random: action.payload}
-            }
-        },
-        toggleRepeat: {
-            reducer(state, action) {
-                return {...state, repeat: action.payload}
-            }
-        },
-        togglePlaying: {
-            reducer(state, action) {
-                return {...state, playing: action.payload}
-            }
+      setSongsList: {
+        reducer(state, action){
+          return {...state, songList: action.payload}
         }
-    }
+      },
+      setCurrentSong: {
+          reducer(state, action) {
+              return {
+                  ...state, 
+                  currentSongId: action.payload._id, 
+                  currentSongIndex: action.payload.songIndex,
+                  playing: true
+              }
+          }
+      },
+      toggleShuffle: {
+          reducer(state, action) {
+              return {...state, ...action.payload};
+          }
+      },
+      toggleRepeat: {
+        reducer(state, action) {
+          return {...state, ...action.payload};
+        }
+      },
+      togglePlaying: {
+        reducer(state, action) {
+          return {...state, ...action.payload};
+        }
+      },
+      playPreviousSong: {
+        reducer(state, action) {
+          let previous;
+          if (state.songs.currentSongIndex === 0) {
+            previous = { currentSongIndex: state.songs.songList.length - 1, _id: state.songs.songList[state.songs.songList.length - 1] }
+          } else {
+            previous = { songIndex: state.songs.currentSongIndex - 1, _id: state.songs.songList[state.songs.currentSongIndex - 1]._id }
+          }
+          return { ...state, ...previous };
+
+        }
+      }
+  }
 })
 
 
-export const getSongs = (state) => state.songs
-export const getCurrentSong = (state) => state.songs.currentSong;
+export const getSongState = (state) => state.songs
+export const getCurrentSong = (state) => state.songs.currentSongId;
+export const getCurrentSongIndex = (state) => state.songs.currentSongIndex;
+export const getSongList = (state) => state.songs.songList;
 
-export const { setSongsList, setCurrentSong, setRandom, toggleRepeat, togglePlaying } = songsSlice.actions
+export const { setSongsList, setCurrentSong, toggleShuffle, toggleRepeat, togglePlaying, playPreviousSong } = songsSlice.actions
 
 
 
