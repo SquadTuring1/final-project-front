b374ca4e5495e1ca930596fcb9822c58337dfdfa
jose-cuevas <jgutifer@gmail.com>
2022-10-25 12:31:54 +0200

    pop over and model problems

--- src/components/PopoverSongCover/PopoverSongCover.jsx
+++ src/components/PopoverSongCover/PopoverSongCover.jsx
@@ -14,15 +14,62 @@ import {
   PopMenu,
   PopCoverItems,
   PopItems,
+  Button,
 } from '../../ui/index';
 import PopoverNavbar from '../PopoverNavbar';
+import ReactModal from 'react-modal';
+
+
+// Styles for add to playlist Modal
+const customStyles = {
+  content: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    width: '30%',
+    border: 'none',
+    fontFamily: 'Source Sans Pro, sans-serif',
+    color: 'white',
+    backgroundColor: '#040810',
+    top: '50%',
+    left: '50%',
+    // right: 'auto',
+    bottom: 'auto',
+    // marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    // overflow: hidden
+  },
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.7)'
+  }
+};
 
 const PopoverSongCover = () => {
   const [isPopOpen, setIsPopOpen] = useState(false);
   const navigate = useNavigate();
 
+  
+
+  const [modalOpen, setModalOpen] = useState(false)
+  const openModal = () => {
+    setModalOpen(true)
+  }
+  const closeModal = () => {
+    setModalOpen(false)
+  }
+  const handlePopOver = () => {
+    setModalOpen(false)
+      }
+
+const handleDelete = () => {
+  console.log("sdjksjdkjsd")
+  setIsPopOpen(false)
+  openModal()
+}
   return (
     <Popover
+    onclick={handlePopOver}
       isOpen={isPopOpen}
       onClickOutside={() => setIsPopOpen(false)}
       positions={['bottom']} // preferred positions by priority
@@ -35,10 +82,19 @@ const PopoverSongCover = () => {
             <PopItems>
               <RiEditBoxLine /> Edit
             </PopItems>
-            <PopItems>
-              <RiDeleteBinLine />
-              Delete
+            <PopItems onClick={handleDelete}>             
+              
+             <RiDeleteBinLine  />Delete 
             </PopItems>
+            <PopItems onClick={openModal}>+ Add to playlist</PopItems>
+            <ReactModal style={customStyles} isOpen={modalOpen} onRequestClose={closeModal}>  
+            <p>Jose</p>        
+              <PopItems>Add to Playlist</PopItems>
+              <PopItems>Create Playlist</PopItems>
+              <PopItems onClick={closeModal}>Close</PopItems>
+              
+            </ReactModal>
+            {/* <Button onClick={openModal}>+ Add to playlist</Button> */}
           </PopCoverItems>
         </PopMenu>
       }
