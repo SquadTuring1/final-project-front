e1986a86f1b232e41f6e230e00239dc150f4795a
blakejohns5 <100588924+blakejohns5@users.noreply.github.com>
2022-10-28 01:36:05 +0200

    basic implementation of drag-and-drop in playlist, local state only

--- src/pages/Playlist/Playlist.jsx
+++ src/pages/Playlist/Playlist.jsx
@@ -1,36 +1,79 @@
 import { useSelector } from "react-redux";
 import { PlaylistColumn, PlaylistContainer, PlaylistTitle, PlaylistInfo, PlaylistCoverSm, PlaylistSong, PlaylistColumnSongs, CoverSong, CoverSongTitle } from "../../ui/index"
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { getAuthUser } from "../../features/auth/authSlice";
 import { useGetPlaylistsQuery, useLazyGetSinglePlaylistQuery } from "../../features/api/apiSlice";
 import Scrollbars from "react-custom-scrollbars-2";
 import SongItem from '../Dashboard/SongItem/index'
 import PopoverSongCover from "../../components/PopoverSongCover";
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd/'
+import { nanoid } from '@reduxjs/toolkit'
 
 
 const Playlist = () => {
   const userId = useSelector(getAuthUser);
   // const songList = useSelector(getSongList)
-  const [ selectedPlaylistId,  setSelectedPlaylistId ] = useState(null);
+  const [ itemList, updateItemList ] = useState(null);
+  
 
   const { data: playlistsData, isLoading: isPlaylistsLoading, isSuccess: isPlaylistsSuccess, isError: isPlaylistsError, error: playlistsError } = useGetPlaylistsQuery();
-  const [ handleClick, {
+  const [ getSinglePlaylist, {
     data: clickedPlaylist, 
+    isFetching: isClickedFetching,
     isLoading: isClickedLoading, 
     isSuccess: isClickedSuccess, 
     isError: isClickedError, 
     error: clickedError,
   }]  = useLazyGetSinglePlaylistQuery()
+
+  const handleClick = async (playlistId) => {
+    await getSinglePlaylist(playlistId);
+    if (isClickedLoading) {
+      console.log('playlist loading')
+    } else if (isClickedSuccess) {
+      updateItemList(clickedPlaylist?.playlist.songs)
+    }
+  }
+
+  useEffect(() => {
+    updateItemList(clickedPlaylist?.playlist.songs)
+  }, [clickedPlaylist])
+
+
+  const draggableStyles = {
+    userSelect: 'none',
+    padding: 16,
+    margin: '0 0 8px 0',
+    minHeight: '50px',
+    color: 'white',
+  }
   
-  
-  
-  
+  const handleOnDragEnd = (result) => {
+    const { source, destination, draggableId } = result;
+    
+    if (!destination) return;
+    
+    if (destination.droppableId === source.droppableId && destination.index === source.index) {
+      return;
+    }
+    
+    if (isClickedSuccess) {  
+      const songsArray = Array.from(clickedPlaylist.playlist.songs);
+      const [ reorderedItem ] = songsArray.splice(result.source.index, 1);
+      songsArray.splice(result.destination.index, 0, reorderedItem)
+      updateItemList(songsArray)
+    }
+    // 
+    // console.log(columnIndex)
+    console.log(source, destination)
+  }
   
   let playlistsContent;
   if (isPlaylistsLoading) {
     playlistsContent = 
         <div>Playlists are being loaded...</div>
   } else if (isPlaylistsSuccess) {
+
     playlistsContent = 
       <>
       {playlistsData?.playlists?.map(playlist =>
@@ -43,39 +86,71 @@ const Playlist = () => {
   }
 
   let songsContent;
-  if (isClickedLoading) {
+  if (isClickedLoading || isClickedFetching) {
     songsContent = <div>No playlist selected</div>
   } else if (isClickedSuccess) {
-    console.log(clickedPlaylist)
+    
     // dispatch(setSongsList({songList: clickedPlaylist, currentSongIndex: 0, currentSongId: clickedPlaylist[0]?._id, currentSongUrl: List[0]?.fileUrl, playing: false}));
-    // console.log(songList)
-    songsContent = 
-      clickedPlaylist?.playlist?.songs.map(({ imageUrl, genre, title, artist, likedBy, _id}, index) => 
-        <PlaylistSong key={_id}>
-          <CoverSongTitle className="index__song--playlist">{index+1}</CoverSongTitle>
-          <CoverSong className="cover__song--playlist" src={imageUrl} />
-          <CoverSongTitle className="title__song--playlist playlist__info--row">{title}</CoverSongTitle>
-          <CoverSongTitle className="artist__song--playlist playlist__info--row">{artist}</CoverSongTitle>
-          <CoverSongTitle className="genre__song--playlist playlist__info--row">{genre.title}</CoverSongTitle>
-        </PlaylistSong>
+    
+    songsContent =       
+      itemList?.map(({ imageUrl, genre, title, artist, likedBy, _id}, index) =>    
+        <Draggable key={_id} draggableId={_id} index={index}>
+          {(provided, snapshot) => {
+            return (
+              <PlaylistSong 
+                {...provided.draggableProps} 
+                ref={provided.innerRef}
+                {...provided.dragHandleProps} 
+                style={{...draggableStyles, backgroundColor: snapshot.isDragging && '#456C86', ...provided.draggableProps.style}} >
+                <CoverSongTitle className="index__song--playlist">{index+1}</CoverSongTitle>
+                <CoverSong className="cover__song--playlist" src={imageUrl} />
+                <CoverSongTitle className="title__song--playlist playlist__info--row">{title}</CoverSongTitle>
+                <CoverSongTitle className="artist__song--playlist playlist__info--row">{artist}</CoverSongTitle>
+                <CoverSongTitle className="genre__song--playlist playlist__info--row">{genre.title}</CoverSongTitle>
+              </PlaylistSong>
+            )
+          }}
+        </Draggable>
         )
   }
-
+  
   return (
     <>  
     <Scrollbars universal>
       <PlaylistContainer>
         <Scrollbars universal>
-          <PlaylistColumn className="playlist__covers">
-    {/* <h1>My Playlist</h1> */}
-            {playlistsContent}
-          </PlaylistColumn>
+          <DragDropContext onDragEnd={handleOnDragEnd}>
+            <Droppable droppableId='playlists' key={nanoid()}>
+              {(provided, snapshot) => (
+                  <PlaylistColumn className="playlist__covers"
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  >
+                    {playlistsContent}
+                    {provided.placeholder}  
+                  </PlaylistColumn>
+                  
+                )
+                
+              }
+            </Droppable>
+          </DragDropContext>
         </Scrollbars>
         <Scrollbars>
-          <PlaylistColumnSongs className="playlist__songs">
-            {songsContent}
-            {/* {playlistsContent} */}
-          </PlaylistColumnSongs>
+          <DragDropContext onDragEnd={handleOnDragEnd}>
+            <Droppable droppableId='tracks' key={nanoid()}>
+              {(provided, snapshot) => (
+                  <PlaylistColumnSongs className="playlist__songs"
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  >
+                    {songsContent }
+                    {provided.placeholder}  
+                  </PlaylistColumnSongs>
+                )
+              }
+            </Droppable>
+          </DragDropContext>
         </Scrollbars>
       </PlaylistContainer>
     </Scrollbars>
