38c6e96f4c8d0da9f5617cf0572ad0df5588fd4f
Sefi Cohen <67965039+seficohen1@users.noreply.github.com>
2022-10-29 21:32:53 +0200

    feat(redux-persist-localStorage): added redux persist dependency to save auth state in local storage

--- src/app/store.js
+++ src/app/store.js
@@ -3,17 +3,29 @@ import authReducer from '../features/auth/authSlice';
 import songsReducer from '../features/songs/songsSlice';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
 import { apiSlice } from '../features/api/apiSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import { combineReducers } from '@reduxjs/toolkit';
 
+const persistConfig = {
+  key: 'root',
+  blacklist: ['songs', 'api'],
+  storage,
+};
+
+const reducer = combineReducers({
+  auth: authReducer,
+  // users: usersReducer,
+  songs: songsReducer,
+  [apiSlice.reducerPath]: apiSlice.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    // users: usersReducer,
-    songs: songsReducer,
-    [apiSlice.reducerPath]:apiSlice.reducer
-  },
-  middleware: getDefaultMiddleware => 
-    getDefaultMiddleware().concat(apiSlice.middleware)
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 // enable refetchOnFocus / refetchOnReConnect
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import { store } from './app/store'
-import { Provider } from 'react-redux'
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 
+let persistStor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>      
+    <BrowserRouter>
       <Provider store={store}>
+        <PersistGate persistor={persistStor}>
           <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
