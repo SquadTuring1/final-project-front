8bcf71100e9724dfecb02066a07ca974c188fb1f
Sofia Romera <sofia-romera@outlook.com>
2022-10-30 23:04:28 +0100

    change swiper config

--- src/pages/Dashboard/SongList/SongList.jsx
+++ src/pages/Dashboard/SongList/SongList.jsx
@@ -6,10 +6,11 @@ import { setCurrentSong, setSongsList } from '../../../features/songs/songsSlice
 import { useDispatch } from 'react-redux';
 import { useState } from 'react'
 
+import Swiper, { Navigation, Pagination } from 'swiper';
 import "swiper/css";
 import "swiper/css/bundle";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { A11y, Navigation, Pagination, Scrollbar, Virtual } from "swiper";
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import { A11y, Navigation, Pagination, Scrollbar, Virtual } from "swiper";
 
 
 
@@ -29,36 +30,73 @@ const SongList = () => {
 
 
 
+  // let content;
+  // if (isLoading) {
+  //   content = <p>Loading...</p>
+  // } else if (isSuccess) {
+  //   // console.log(songList)
+  //   content = songList?.map(({ _id, imageUrl, artist, title, fileUrl, likedBY, album }, songIndex) =>
+  //     <SwiperSlide key={_id} virtualIndex={_id}>
+  //       <SongItem artist={artist?.artistName} title={title} cover={imageUrl} songId={_id} songIndex={songIndex} fileUrl={fileUrl} likedBY={likedBY} album={album} />  
+  //     </SwiperSlide> 
+  //   )
+  // } else if (isError) {
+  //   content = <p>{error}</p>
+  // } 
+  
+
+  const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Pagination],
+    slidesPerView: 4,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+    },
+  });
+  
   let content;
   if (isLoading) {
     content = <p>Loading...</p>
   } else if (isSuccess) {
     // console.log(songList)
     content = songList?.map(({ _id, imageUrl, artist, title, fileUrl, likedBY, album }, songIndex) =>
-      <SwiperSlide key={_id} virtualIndex={_id}>
+      <div className='swiper-container' key={_id} virtualIndex={_id}>
         <SongItem artist={artist?.artistName} title={title} cover={imageUrl} songId={_id} songIndex={songIndex} fileUrl={fileUrl} likedBY={likedBY} album={album} />  
-      </SwiperSlide> 
+      </div> 
     )
   } else if (isError) {
     content = <p>{error}</p>
   } 
 
   return (
-    <Swiper
-      modules={[ Navigation, Pagination, Virtual]}
-      navigation
-      spaceBetween={10}
-      slidesPerView={9}
-      virtual
-      style={{
-        "--swiper-navigation-color": "#fff",
-        "--swiper-navigation-size": "1.5rem"
-      }}
-    >
-      <SongsDash>
+    // <Swiper
+    //   modules={[ Navigation, Pagination, Virtual]}
+    //   navigation
+    //   spaceBetween={10}
+    //   slidesPerView={9}
+    //   virtual
+    //   style={{
+    //     "--swiper-navigation-color": "#fff",
+    //     "--swiper-navigation-size": "1.5rem"
+    //   }}
+    // >
+    //   <SongsDash>
+    //     {content}
+    //   </SongsDash>
+    // </Swiper>
+
+    <div className="swiper">
+      <div className="swiper-wrapper">
         {content}
-      </SongsDash>
-    </Swiper>
+		  </div>
+      <div class="swiper-button-next" style={{backgroundColor: 'white', width: '20px'}}></div>
+      <div class="swiper-button-prev" style={{backgroundColor: 'white', width: '20px'}}></div>
+	</div>
   );
 };
 
